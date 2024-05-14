1ef7c863bb1037478381758b379d096ad167a305
Stach13 <112652730+Stach13@users.noreply.github.com>
2024-05-14 17:34:21 -0400

    Created photography slideshow

---
+++ docs/photo-slideshow.js
@@ -0,0 +1,30 @@
+
+
+var i = 0
+var images = [];
+
+images[0] = 'images/album/golf_friends_final.jpg'
+images[1] = 'images/album/18th-hole.jpg'
+images[2] = 'images/album/bluff-cliff.JPG'
+images[3] = 'images/album/bluff-stream.JPG'
+images[4] = 'images/album/eclipse.JPG'
+images[5] = 'images/album/flower.JPG'
+images[6] = 'images/album/icicles.JPG'
+images[7] = 'images/album/koepka.jpg'
+images[8] = 'images/album/leaf.JPG'
+images[9] = 'images/album/mushroom2.JPG'
+images[10] = 'images/album/rec-hall.JPG'
+
+
+function changeImg(){
+    document.slide.src = images[i];
+
+    if (i < images.length){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout('changeImg()', 4 * 1000);
+}
+window.onload = changeImg;
