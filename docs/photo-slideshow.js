eece45e90d065ef08e26c984c46f9caa2ac51c90
Stach13 <112652730+Stach13@users.noreply.github.com>
2024-05-15 19:50:56 -0400

    Added captions to slideshow

--- docs/photo-slideshow.js
+++ docs/photo-slideshow.js
@@ -1,24 +1,37 @@
 
-
 var i = 0
 var images = [];
+var caption = []
 
-images[0] = 'images/album/golf_friends_final.jpg'
-images[1] = 'images/album/18th-hole.jpg'
-images[2] = 'images/album/bluff-cliff.JPG'
-images[3] = 'images/album/bluff-stream.JPG'
-images[4] = 'images/album/eclipse.JPG'
-images[5] = 'images/album/flower.JPG'
-images[6] = 'images/album/icicles.JPG'
-images[7] = 'images/album/koepka.jpg'
-images[8] = 'images/album/leaf.JPG'
-images[9] = 'images/album/multnomah.JPG'
-images[10] = 'images/album/rec-hall.JPG'
-images[11] = 'images/album/press.jpg'
+//caption[0] = 'golf friends'
+caption[0] = 'Fans on the 18th Hole PGA'
+caption[1] = 'Bluff Cliff'
+caption[2] = 'Ground Runoff'
+caption[3] = 'Solar Eclipse 2024'
+caption[4] = 'Flower'
+caption[5] = 'Icicles on Branch at Bluffs'
+caption[6] = 'Brooks Koepka'
+caption[7] = 'Leaf'
+caption[8] = 'The Columbia River at Multnomah Falls'
+caption[9] = 'Rec Hall at Penn State'
+caption[10] = 'Wide Shot of Koepka and the Media'
 
+//images[0] = 'images/album/golf_friends_final.jpg'
+images[0] = 'images/album/18th-hole.jpg'
+images[1] = 'images/album/bluff-cliff.JPG'
+images[2] = 'images/album/bluff-stream.JPG'
+images[3] = 'images/album/eclipse.JPG'
+images[4] = 'images/album/flower.JPG'
+images[5] = 'images/album/icicles.JPG'
+images[6] = 'images/album/koepka.jpg'
+images[7] = 'images/album/leaf.JPG'
+images[8] = 'images/album/multnomah.JPG'
+images[9] = 'images/album/rec-hall.JPG'
+images[10] = 'images/album/press.jpg'
 
 function changeImg(){
     document.slide.src = images[i];
+    document.querySelector('.caption').innerHTML = caption[i];
 
     if (i < images.length - 1){
         i++;
@@ -26,6 +39,13 @@ function changeImg(){
         i = 0;
     }
 
-    setTimeout('changeImg()', 4 * 1000);
+    if (i < caption.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout('changeImg()', 5 * 1000);
 }
+
 window.onload = changeImg;
